dc8838efd89fdb7bc9fe9ed0e11f010341cf610f
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-03-19 22:09:11 +0300

    Editing hierarchical data grid to have field names, adding scroll view for menu

--- Model/HierarchicalTreeDataGridItem.cs
+++ Model/HierarchicalTreeDataGridItem.cs
@@ -6,15 +6,22 @@ public class HierarchicalTreeDataGridItem
     {
         Name = product.Name;
         Children = new List<HierarchicalTreeDataGridItem>();
-        Children.Add(new HierarchicalTreeDataGridItem(product.ManufacturerName));
-        Children.Add(new HierarchicalTreeDataGridItem(product.ManufacturerUnp));
-        Children.Add(new HierarchicalTreeDataGridItem(product.StorageQuantity.ToString()));
-        Children.Add(new HierarchicalTreeDataGridItem(product.Address));
+        Children.Add(new HierarchicalTreeDataGridItem("Название производителя", product.ManufacturerName));
+        Children.Add(new HierarchicalTreeDataGridItem("УНП производителя", product.ManufacturerUnp));
+        Children.Add(new HierarchicalTreeDataGridItem("Количество на складе", product.StorageQuantity.ToString()));
+        Children.Add(new HierarchicalTreeDataGridItem("Адрес", product.Address));
     }
 
-    public HierarchicalTreeDataGridItem(string value)
+    private HierarchicalTreeDataGridItem(string value, string childValue)
     {
         Name = value;
+        Children = [new HierarchicalTreeDataGridItem(childValue)];
+    }
+
+    private HierarchicalTreeDataGridItem(string value)
+    {
+        Name = value;
+        Children = [];
     }
 
     public string? Name { get; set; }
